c7d3abaee2674c30ab37a967ad211ae796fdce23
areeb.rafiq <areeb.rafiq@paysyslabs.com>
2025-11-07 18:59:04 +0500

    adding components and pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
+import './App.css'
 import Home from './Pages/Home'
 import Chats from './Pages/Chats'
 function App() {
   return (
-   <div>
+   <div className='App'>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/chats' element={<Chats/>}/>
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Box,
+  Text,
+  Container,
+} from "@chakra-ui/react";
+import Signup from "../Components/Signup";
+import Login from "../Components/Login";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        alignItems={"center"}
+        p={4}
+        bg="white"
+        w={"100%"}
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="2px"
+      >
+        <Text fontSize="4xl" color="black">
+          Chat Application
+        </Text>
+      </Box>
+      <Box p={4} bg="white" w={"100%"} borderRadius="lg" borderWidth="2px">
+        <Tabs variant="soft-rounded">
+        <TabList>
+          <Tab width={"50%"}>Login</Tab>
+          <Tab width={"50%"}>Sign Up</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel><Login/></TabPanel>
+          <TabPanel><Signup/></TabPanel>
+        </TabPanels>
+      </Tabs>
+      </Box>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
